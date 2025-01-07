028785c6c32ec781ef4ed48b55a1981f723607a4
yann.clarisse <yann.clarisse@epita.fr>
2025-01-07 15:16:31 +0100

    fix: lexer.h

--- src/lexer/lexer.h
+++ src/lexer/lexer.h
@@ -3,7 +3,6 @@
 
 #include "token.h"
 
-
 struct lexer
 {
     const char *data;
@@ -12,5 +11,15 @@ struct lexer
 };
 
 struct lexer *lexer_new(const char *data);
+
+void lexer_free(struct lexer *lexer);
+
+struct lexer *lexer_next(struct lexer *lexer);
+
+struct lexer *lexer_peek(struct lexer *lexer);
+
+struct lexer *lexer_pop(struct lexer *lexer);
+
+void lexer_refill(struct lexer *lexer);
 
 #endif // LEXER_H
