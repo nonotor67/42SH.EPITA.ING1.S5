f9fa29b650b4203234227b5a12e578780a102541
Antoine Blumenroeder <antoine.blumenroeder@epita.fr>
2025-01-08 15:27:54 +0100

    fix: name change and single test

--- tests/test_io.c
+++ tests/test_io.c
@@ -1,43 +1,24 @@
+#include "../src/io/io.h"
+
 #include <criterion/criterion.h>
-#include <criterion/redirect.h>
-#include <io/io.h>
 #include <unistd.h>
 
 Test(read_input, test_read_input_from_file)
 {
-    char *argv[] = { "./bin/42sh", "input.txt" };
-    char *result = read_input(2, argv);
-    char *expected = "Hello, World!\n";
-    cr_assert_str_eq(result, expected, "Expected %s, but got %s", expected,
-                     result);
+    char *argv[] = {"./bin/42sh", "input.txt"};
+    struct reader *reader = reader_new(sizeof(argv) / sizeof(char *), argv);
+    char *result = malloc(100);
+    int i = 0;
+    while (1)
+    {
+        int c = reader_next(reader);
+        if (c == EOF)
+            break;
+        result[i++] = c;
+    }
+    result[i] = '\0';
+    reader_free(reader);
+    char *expected = "Hello, World!\nI am a test file.\n";
+    cr_assert_str_eq(result, expected, "Expected %s, but got %s", expected, result);
     free(result);
 }
-
-Test(read_input, test_read_input_from_string)
-{
-    char *argv[] = { "./bin/42sh", "-c", "Hello, World!\n" };
-    char *result = read_input(3, argv);
-    char *expected = "Hello, World!\n";
-    cr_assert_str_eq(result, expected, "Expected %s, but got %s", expected,
-                     result);
-    free(result);
-}
-
-/*
-Test(read_from_stdin, valid_input)
-{
-    cr_redirect_stdin();
-
-    const char *input_data = "Hello, world!\nThis is a test.\n";
-    fputs(input_data, stdin);
-
-    rewind(stdin);
-
-    char *result = read_from_stdin();
-
-    cr_assert_not_null(result, "La fonction doit retourner une chaîne valide.");
-    cr_assert_str_eq(result, input_data, "La sortie attendue est identique à
-l'entrée.");
-
-    free(result);
-} */
