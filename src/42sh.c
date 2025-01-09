469dffb91eefac033dbcf51f3907ad417d81c758
Noah CONTAL <noah.contal@epita.fr>
2025-01-09 18:08:13 +0100

    feat: add main

--- src/42sh.c
+++ src/42sh.c
@@ -1,23 +1,30 @@
+#include <ast/ast.h>
+#include <execution/execution.h>
+#include <io/io.h>
+#include <lexer/lexer.h>
+#include <parser/parser.h>
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#include "io/io.h"
-#include "lexer/lexer.h"
-#include "parser/parser.h"
-#include "ast/ast.h"
-#include "execution/execution.h"
-
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
-    //Create a reader, lexer, and parser.
+    // Create a reader, lexer, and parser.
     struct reader *reader = reader_new(argc, argv);
     struct lexer *lexer = lexer_new(reader);
     struct parser *parser = parser_new(lexer);
 
-    //Parse the input.
+    // Parse the input.
     struct ast *ast = parse(parser);
 
-    //Execute the AST.
-    return execution(ast);
+    // Execute the AST.
+    int res = execution(ast);
+
+    reader_free(reader);
+    lexer_free(lexer);
+    parser_free(parser);
+    ast_free(ast);
+
+    return res;
 }
