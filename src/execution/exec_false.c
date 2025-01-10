aae39886a9d465f99ec2338ef2478efef45745ca
noah.contal <noah.contal@epita.fr>
2025-01-10 16:39:24 +0100

    fix: clang format

--- src/execution/exec_false.c
+++ src/execution/exec_false.c
@@ -2,7 +2,8 @@
 
 #include "execution.h"
 
-int exec_false(__attribute__((unused)) int argc, __attribute__((unused)) char **argv)
+int exec_false(__attribute__((unused)) int argc,
+               __attribute__((unused)) char **argv)
 {
     return 1;
 }
