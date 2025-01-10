e9dba55857bd1b5b5974e2c95817da944aeb24f3
noah.contal <noah.contal@epita.fr>
2025-01-10 16:36:04 +0100

    fix: no check error

--- src/execution/exec_false.c
+++ src/execution/exec_false.c
@@ -4,11 +4,5 @@
 
 int exec_false(int argc, __attribute__((unused)) char **argv)
 {
-    if (argc > 1)
-    {
-        fprintf(stderr, "false: too many arguments\n");
-        return 1;
-    }
-
     return 1;
 }
