23f1ce0520e0b5ae06f2536c398a2a3f40df4d63
noah.contal <noah.contal@epita.fr>
2025-01-13 18:06:58 +0100

    feat: tkt

--- src/42sh.c
+++ src/42sh.c
@@ -3,9 +3,33 @@
 #include <io/io.h>
 #include <lexer/lexer.h>
 #include <parser/parser.h>
+#include <stddef.h>
+#include <utils/utils.h>
+
+static struct HashMap *init_hashmap(void)
+{
+    // Create a new hash table
+    struct HashMap *ht = create_hash_table();
+    if (!ht)
+    {
+        fprintf(stderr, "42sh: failed to create the hash table\n");
+        exit(1);
+    }
+
+    // Insert the default variables
+    insertVariable(ht, "PWD", getenv("PWD"));
+
+    return ht;
+}
 
 int main(int argc, char **argv)
 {
+    struct HashMap *ht = init_hashmap();
+    if (!ht)
+    {
+        fprintf(stderr, "42sh: failed to initialize the hash table\n");
+        exit(1);
+    }
     // Create a reader, lexer, and parser.
     struct reader *reader = reader_new(argc, argv);
     struct lexer *lexer = lexer_new(reader);
@@ -29,6 +53,8 @@ int main(int argc, char **argv)
 
     reader_free(reader);
     lexer_free(lexer);
+
+    free_hash_map(ht);
 
     return res;
 }
