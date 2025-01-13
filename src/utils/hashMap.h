d306dcf1ba1077337a9f86be2617e4e37e8be086
noah.contal <noah.contal@epita.fr>
2025-01-13 15:06:16 +0100

    fix: add comments

--- src/utils/hashMap.h
+++ src/utils/hashMap.h
@@ -10,13 +10,50 @@ struct HashMap
     struct Variable *map[TABLE_SIZE];
 };
 
+/*
+    @brief Create a new hash table
+    @return A pointer to the newly created hash table
+    @note The hash table is initialized with NULL values
+ */
 struct HashMap *create_hash_table();
+/*
+    @brief Free the memory allocated for the hash table
+    @param ht The hash table to free
+ */
 void free_hash_map(struct HashMap *ht);
 
+/*
+    @brief Hash a string
+    @param str The string to hash
+    @return The hash value
+ */
 unsigned int hash(char *str);
+/*
+    @brief Insert a new variable in the hash table
+    @param ht The hash table
+    @param name The name of the variable
+    @param value The value of the variable
+ */
 void insertVariable(struct HashMap *ht, char *name, char *value);
+/*
+    @brief Get a variable from the hash table
+    @param ht The hash table
+    @param name The name of the variable
+    @return The variable
+ */
 struct Variable getVariable(struct HashMap *ht, char *name);
+/*
+    @brief Remove a variable from the hash table
+    @param ht The hash table
+    @param name The name of the variable to remove
+ */
 void removeVariable(struct HashMap *ht, char *name);
+/*
+    @brief Update the value of a variable in the hash table
+    @param ht The hash table
+    @param name The name of the variable to update
+    @param value The new value of the variable
+ */
 void updateVariable(struct HashMap *ht, char *name, char *value);
 
 #endif /* !HASHMAP_H */
--- src/utils/variable.h
+++ src/utils/variable.h
@@ -8,7 +8,17 @@ struct Variable
     struct Variable *next; // Collisions are handled with a linked list
 };
 
+/*
+    @brief Create a new variable
+    @param name The name of the variable
+    @param value The value of the variable
+    @return A pointer to the newly created variable
+ */
 struct Variable *create_variable(char *name, char *value);
+/*
+    @brief Free the memory allocated for a variable
+    @param var The variable to free
+ */
 void free_variable(struct Variable *var);
 
 #endif /* !VARIABLE_H */
