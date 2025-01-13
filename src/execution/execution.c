d5901fa540eaf309c7b4c2a9b8f718cb935051dd
jules.ladaigue <jules.ladaigue@epita.fr>
2025-01-13 12:41:39 +0100

    fix: clang-format

--- src/execution/execution.c
+++ src/execution/execution.c
@@ -2,24 +2,27 @@
 
 typedef int (*execute_function)(struct ast *);
 
-struct execute_entry {
+struct execute_entry
+{
     enum ast_type type;
     execute_function func;
 };
 
 static const struct execute_entry execute_table[] = {
-    {SIMPLE_COMMAND, execute_command},
-    {COMMAND_LIST, execute_command_list},
-    {CONDITIONS, execute_condition},
+    { SIMPLE_COMMAND, execute_command },
+    { COMMAND_LIST, execute_command_list },
+    { CONDITIONS, execute_condition },
 };
 
 int execute_node(struct ast *node)
 {
+    static const size_t table_size =
+        sizeof(execute_table) / sizeof(execute_table[0]);
 
-    static const size_t table_size = sizeof(execute_table) / sizeof(execute_table[0]);
-
-    for (size_t i = 0; i < table_size; i++) {
-        if (execute_table[i].type == node->type) {
+    for (size_t i = 0; i < table_size; i++)
+    {
+        if (execute_table[i].type == node->type)
+        {
             return execute_table[i].func(node);
         }
     }
