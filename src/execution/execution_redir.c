fc93ec85118dcca6235e4303a44fcb6abda6ed80
jules.ladaigue <jules.ladaigue@epita.fr>
2025-01-15 17:27:38 +0100

    feat: add some redirs

--- src/execution/execution_redir.c
+++ src/execution/execution_redir.c
@@ -5,56 +5,106 @@
 
 #include "execution.h"
 
+// Handle >
 static int simple_redir(char *filename, int io_number, struct ast *ast)
 {
+    if (io_number == -1)
+    {
+        io_number = STDOUT_FILENO;
+    }
     // Remove the file if it exists
     if (access(filename, F_OK) != -1)
     {
         remove(filename);
     }
+    // Create and open the file
     int fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0644);
     if (fd == -1)
     {
-        exit(1);
+        perror("open");
+        return -1;
     }
-    int dupstdout = dup(STDOUT_FILENO);
-    if (dupstdout == -1)
+    // Redirect the file descriptor
+    if (dup2(fd, io_number) == -1)
     {
+        perror("dup2");
         close(fd);
-        exit(1);
+        return -1;
     }
-    int res = dup2(fd, io_number);
-    if (res == -1)
+    // Close the file descriptor
+    close(fd);
+    // Execute the command
+    int status = dispatch_command(ast);
+    // Restore the file descriptor
+    if (dup2(STDOUT_FILENO, io_number) == -1)
     {
+        perror("dup2");
+        return -1;
+    }
+
+    return status;
+}
+
+// Handle <
+static int input_redir(char *filename, int io_number, struct ast *ast)
+{
+    if (io_number == -1)
+    {
+        io_number = STDIN_FILENO;
+    }
+    // Open the file
+    int fd = open(filename, O_RDONLY);
+    if (fd == -1)
+    {
+        perror("open");
+        return -1;
+    }
+    // Redirect the file descriptor
+    if (dup2(fd, io_number) == -1)
+    {
+        perror("dup2");
         close(fd);
-        close(dupstdout);
-        exit(1);
+        return -1;
     }
-    pid_t pid = fork();
-    if (pid == -1)
+    // Close the file descriptor
+    close(fd);
+    // Execute the command
+    int status = dispatch_command(ast);
+    // Restore the file descriptor
+    if (dup2(STDIN_FILENO, io_number) == -1)
     {
-        exit(1);
-    }
-    if (pid == 0)
-    {
-        exit(dispatch_command(ast));
+        perror("dup2");
+        return -1;
     }
 
-    int status;
-    waitpid(pid, &status, 0);
-    res = dup2(dupstdout, io_number);
-    if (res == -1)
+    return status;
+}
+
+// Handle >&
+static int fd_redir(int fd, int io_number, struct ast *ast)
+{
+    if (io_number == -1)
     {
-        close(dupstdout);
-        exit(1);
+        io_number = STDOUT_FILENO;
+    }
+    // Redirect the file descriptor
+    if (dup2(fd, io_number) == -1)
+    {
+        perror("dup2");
+        close(fd);
+        return -1;
+    }
+    // Close the file descriptor
+    close(fd);
+    // Execute the command
+    int status = dispatch_command(ast);
+    // Restore the file descriptor
+    if (dup2(STDOUT_FILENO, io_number) == -1)
+    {
+        perror("dup2");
+        return -1;
     }
 
-    close(dupstdout);
-
-    if (WIFEXITED(status) && WEXITSTATUS(status) == 127)
-    {
-        exit(1);
-    }
     return status;
 }
 
@@ -62,7 +112,7 @@ static int simple_redir(char *filename, int io_number, struct ast *ast)
 // WORD ;
 int exec_redir(struct ast *ast)
 {
-    int io_number = 1;
+    int io_number = -1;
     int i = 0;
     // Parse io number
     if (isdigit(ast->expanded_redir[0][0]))
@@ -74,12 +124,31 @@ int exec_redir(struct ast *ast)
             ast->expanded_redir[i] = ast->expanded_redir[i + 2];
         }
     }
-    // Handle >
+
     int status = 1;
-    if (strcmp(ast->expanded_redir[i], ">") == 0)
+    if (strcmp(ast->expanded_redir[i], ">") == 0 || strcmp(ast->expanded_redir[i], ">|") == 0)
     {
         status = simple_redir(ast->expanded_redir[i + 1], io_number, ast);
     }
+    else if (strcmp(ast->expanded_redir[i], ">&") == 0)
+    {
+        status = fd_redir(atoi(ast->expanded_redir[i + 1]), io_number, ast);
+    }
+    else if (strcmp(ast->expanded_redir[i], "<>") == 0)
+    {
 
+    }
+    else if (strcmp(ast->expanded_redir[i], "<") == 0)
+    {
+        input_redir(ast->expanded_redir[i + 1], io_number, ast);
+    }
+    else if (strcmp(ast->expanded_redir[i], ">>") == 0)
+    {
+
+    }
+    else if (strcmp(ast->expanded_redir[i], "<&") == 0)
+    {
+
+    }
     return status;
 }
