fc67715b7f22e95890cc1639d2dfa699f66c7b73
jules.ladaigue <jules.ladaigue@epita.fr>
2025-01-18 16:29:50 +0100

    feat: 42sh

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -4,6 +4,23 @@
 #include <stdlib.h>
 #include <string.h>
 
+static int is_ast_valid(struct ast *ast)
+{
+    if (!ast)
+        return 1;
+    int valid = 1;
+    if (ast->type == SIMPLE_COMMAND && ast->values && ast->values[0])
+        valid = !is_closing_word(ast->values[0]);
+
+    if (valid && ast->left)
+        valid = is_ast_valid(ast->left);
+    if (valid && ast->middle)
+        valid = is_ast_valid(ast->middle);
+    if (valid && ast->right)
+        valid = is_ast_valid(ast->right);
+    return valid;
+}
+
 /*
 Contains the creation and top layer of the parser
 --> list
@@ -91,11 +108,19 @@ struct ast *input(struct parser *parser)
     if (!eat(parser->lexer, TOKEN_EOL) && !eat(parser->lexer, TOKEN_EOF))
         parser->status = PARSER_UNEXPECTED_TOKEN;
 
-    // CHECK_STATUS(parser, ast, "Error after parsing EOL or EOF (input)\n");
+    CHECK_STATUS(parser, ast, "Error after parsing EOL or EOF (input)\n");
     return ast;
 }
 
 struct ast *parse(struct parser *parser)
 {
-    return input(parser);
+    struct ast *ast = input(parser);
+    if (!is_ast_valid(ast))
+    {
+        parser->status = PARSER_UNEXPECTED_TOKEN;
+        fprintf(stderr, "Error: Unexpected token (closing word)\n");
+        ast_free(ast);
+        return NULL;
+    }
+    return ast;
 }
--- tests/functional_tests/func_tests.sh
+++ tests/functional_tests/func_tests.sh
@@ -11,11 +11,6 @@ run_test_script() {
     script="$1"
     echo "Executing test script: $script"
     ./"$script" $BIN
-    script_exit_code=$?
-    if [ $script_exit_code -ne 0 ]; then
-        failed_tests=$((failed_tests + 1))
-    fi
-    total_tests=$((total_tests + 1))
 }
 
 # Exécuter chaque script de test
@@ -27,15 +22,5 @@ run_test_script "$path_to_tests/test_redirection.sh"
 run_test_script "$path_to_tests/test_script.sh"
 run_test_script "$path_to_tests/test_pipeline.sh"
 run_test_script "$path_to_tests/test_script_stdin.sh"
+run_test_script "$path_to_tests/test_fails.sh"
 
-# Résumé global
-echo
-echo "==== Global Test Summary ===="
-echo "Total scripts: $total_tests"
-echo "Failed scripts: $failed_tests"
-
-if [ $failed_tests -gt 0 ]; then
-    exit 1
-else
-    exit 0
-fi
