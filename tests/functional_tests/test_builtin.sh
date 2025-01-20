c4477673b2e081dd044b161a2d8e304cb8e33b78
jules.ladaigue <jules.ladaigue@epita.fr>
2025-01-20 16:54:01 +0100

    feat: add dot command

--- tests/functional_tests/test_builtin.sh
+++ tests/functional_tests/test_builtin.sh
@@ -7,3 +7,8 @@ $test $BIN "Simple echo test" "echo Hello world"
 $test $BIN "Simple echo test with newline" "echo -n Hello world"
 $test $BIN "Simple true" "true"
 $test $BIN "Simple false" "false"
+
+# Test dot builtin
+
+$test $BIN "Simple dot test" ". ./functional_tests/dot_script/simple_test.sh"
+$test $BIN "Simple variable test" "a=hello;. ./functional_tests/dot_script/variable_test.sh;echo $a; echo $b"
