a07b10ce1e4bba4949810cfc1e3c1ae12cf3a117
jules.ladaigue <jules.ladaigue@epita.fr>
2025-01-21 22:35:33 +0100

    feat: add change dir

--- src/execution/execution_subshell.c
+++ src/execution/execution_subshell.c
@@ -2,12 +2,27 @@
 
 int execute_subshell(struct ast *ast)
 {
+    // Save the current variables
     struct HashMapVar saved_variables = copy_hash_map_var();
+    // Save the current directory
+    // Copy PWD and OLDPWD
+    char *pwd = malloc(strlen(getVariable("PWD").value) + 1);
+    strcpy(pwd, getVariable("PWD").value);
+    char *oldpwd = malloc(strlen(getVariable("OLDPWD").value) + 1);
+    strcpy(oldpwd, getVariable("OLDPWD").value);
 
     int status = execute_node(ast->left);
 
+    // Restore the saved variables
     free_hash_map_var();
     setVariableMap(saved_variables);
+
+    // Restore the current directory
+    insertVariable("PWD", pwd);
+    insertVariable("OLDPWD", oldpwd);
+    chdir(pwd);
+    free(pwd);
+    free(oldpwd);
 
     return status;
 }
--- tests/functional_tests/test_subshell.sh
+++ tests/functional_tests/test_subshell.sh
@@ -9,4 +9,15 @@ $test $BIN "Complicated subshell" "(echo hello | tr a-z A-Z | rev)"
 
 # Test for variable substitution
 $test $BIN "Test with variable substitution" 'a=sh; (a=42; echo -n $a);echo $a'
-$test $BIN "Test with variable substitution" " a=sh; (a=42;b=yann; echo -n $a $b);echo $a $b"
+$test $BIN "Test with variable substitution" 'a=sh; (a=42;b=yann; echo -n \$a \$b);echo \$a \$b'
+
+# Test with multiple subshell
+$test $BIN "Test with multiple subshell" '(echo hello; (echo world))'
+$test $BIN "Test with multiple subshell" '(echo hello; (echo world; echo yann))'
+
+# Test with multiple subshell and variable substitution
+$test $BIN "Test with multiple subshell and variable substitution" 'a=sh; (a=42; echo -n $a);echo $a; (echo hello; (echo world; echo yann))'
+
+# Test subshell with cd
+$test $BIN "Test subshell with cd" '(cd /; pwd); pwd'
+$test $BIN "Test subshell with cd" '(cd /; pwd); pwd; (cd /usr; pwd); pwd'
