3a00be9fdd1a1b7921d48c4ba37e705715cc9348
noah.contal <noah.contal@epita.fr>
2025-01-23 14:49:43 +0100

    fix: exit builtins

--- src/execution/execution_commands.c
+++ src/execution/execution_commands.c
@@ -96,7 +96,7 @@ int dispatch_command(struct ast *ast)
     else if (strcmp(ast->expanded_values[0], "cd") == 0)
         status = exec_cd(argc, ast->expanded_values);
     else if (strcmp(ast->expanded_values[0], "exit") == 0)
-        status = exec_exit(argc, ast->expanded_values);
+        exec_exit(argc, ast->expanded_values);
     else
         status = run_command(real_argv);
 
